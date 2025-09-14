8c70b052bc271cd09f1235b2548e1c2586026c19
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-09-14 03:48:03 +0100

    fix background image

--- src/components/NewHero.tsx
+++ src/components/NewHero.tsx
@@ -20,7 +20,7 @@ const NewHeroSection = () => {
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden pt-24 sm:pt-28 md:pt-32 lg:pt-36 xl:pt-40 2xl:pt-0 pb-4 sm:pb-6 md:pb-6 lg:pb-6  2xl:pb-0 xl:pb-6">
       <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        className="absolute inset-0 bg-contain sm:bg-cover bg-stone-900 bg-center bg-no-repeat"
         style={{
           backgroundImage: `url(${heroImage})`,
         }}
